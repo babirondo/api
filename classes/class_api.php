874b35b7fdb1c8e34e60a4995107e171fd7d11c2
babirondo <babirondo@gmail.com>
2015-12-10 17:28:59 -0200

    respondendo direitinho via json

--- classes/class_api.php
+++ classes/class_api.php
@@ -11,18 +11,22 @@ class api{
 	function Auth($login, $senha, $app){
 		
 	 	$this->con->executa("SELECT * FROM \"JOGADOR\" WHERE \"EMAIL\" = '$login' and \"SENHA\" = '$senha'");
- var_dump($this->con->res);
- 	 	if ($this->con->res === true)
-	 		echo "foi";
-	 	else 
-	 		echo "nao foi";  
-	 
-	  
-	 	
-		$data = array("data"=>array("login" => "$login", 
-								    "senha" => "$senha")
-				
-		  			  );
+ 
+ 	 	if ($this->con->res === true){
+ 	 		//autenticado
+ 	 		
+ 	 		$data = array("data"=>
+ 	 				array(	"email" => $this->con->dados["EMAIL"],
+ 	 						"nome" => $this->con->dados["NOME"])
+ 	 		); 	 	
+ 	 	}
+	 	else {
+	 		// nao encontrado
+	 		$data = array("data"=>
+	 				array(	"erro" => "Nao encontrado" )
+	 		);	 		
+	 	} 
+
 		$app->render ('default.php',$data,200);
 	}
 }
